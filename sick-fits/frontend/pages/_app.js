d513d985220f0067b0f5b97ad1e51173f05df293
Quoc Nguyen <quoc.nguyen@groovetechnology.com>
2022-04-08 15:11:00 +0700

    9

--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -1,6 +1,15 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react/jsx-props-no-spreading */
+import Nprogress from 'nprogress';
+import Router from 'next/router';
 import Page from '../components/Page';
+
+// import 'nprogress/nprogress.css';
+import '../components/styles/nprogress.css';
+
+Router.events.on('routeChangeStart', () => Nprogress.start());
+Router.events.on('routeChangeComplete', () => Nprogress.done());
+Router.events.on('routeChangeError', () => Nprogress.done());
 
 export default function MyApp({ Component, pageProps }) {
   return (
--- sick-fits/frontend/pages/_document.js
+++ sick-fits/frontend/pages/_document.js
@@ -1,9 +1,21 @@
-import Document, { Html, Main, NextScript } from 'next/document';
+import Document, { Html, Main, NextScript, Head } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
+  static getInitialProps({ renderPage }) {
+    const sheet = new ServerStyleSheet();
+    const page = renderPage(
+      // eslint-disable-next-line react/jsx-props-no-spreading
+      (App) => (props) => sheet.collectStyles(<App {...props} />)
+    );
+    const styleTags = sheet.getStyleElement();
+    return { ...page, styleTags };
+  }
+
   render() {
     return (
       <Html lang="en">
+        <Head />
         <body>
           <Main />
           <NextScript />
